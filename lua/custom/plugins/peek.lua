dfc435b43211b1679a28b9f753089035f1f0bb3b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-12-04 03:23:24 +0800

    feat(plugins): add peek

---
+++ lua/custom/plugins/peek.lua
@@ -0,0 +1,13 @@
+-- peek
+-- https://github.com/toppair/peek.nvim
+
+return {
+  'toppair/peek.nvim',
+  event = { 'VeryLazy' },
+  build = 'deno task --quiet build:fast',
+  config = function()
+    require('peek').setup()
+    vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+    vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+  end,
+}
