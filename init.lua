0ceeaf0d9f985679bd4337be9d113b861397f027
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-12-03 08:28:46 +0800

    feat(plugins): add mini.nvim modules for navigation and UI

--- init.lua
+++ init.lua
@@ -929,6 +929,11 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      require('mini.bracketed').setup()
+
+      require('mini.comment').setup()
+
+      require('mini.tabline').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
